d1c219a163f9ff3c8eb21d51cbd430557139533d
CostinMuresan <costin.muresan@gmail.com>
2026-08-31 10:21:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Configurare Supabase
-export const SUPABASE_URL = "https://mxyynercalyookjhwdgs.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_Vx8k1ubEsZAzMue0dLdClA_ANR0qI__";
+export const SUPABASE_URL = "https://https://jwtfoxlxfpydyurtpfmg.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_TRrDBEjb0gTnqBAiR2TymA_6S7OFiL3";
